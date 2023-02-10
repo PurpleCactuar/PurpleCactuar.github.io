04238baab23dfa4cabd2afae35828753aa472dc6
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-02-09 18:02:40 -0800

    Update script.js

--- PROG109/Module6/Artifact3/script.js
+++ PROG109/Module6/Artifact3/script.js
@@ -1,58 +1,47 @@
-	
-// var rHeight =5;
-// var colorEven = "orange";
-// var colorOdd = "black";
-// var symbol ="*";
-
-function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-upRight(pHeight, pColorEven, pColorOdd, pSymbol);
-downRight(pHeight, pColorEven, pColorOdd, pSymbol);
-
-}
-
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=0;i<pHeight;i++){
-rLine +="<p>";
+var rLine  ="";
+       for (let j = 1; j <= i; j++) {
+rLine  +="<p>";
     
 //Create each line on the Rhombus
-for(j=0;j<=i;j++){
+       for (let j = 1; j <= i; j++) {
 
 //Is the position even or odd so we change the color
-if (j%2)
+       if (j % 2 == 0) {
 //even
 rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
+ } else {
 //odd
 rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
+}
 }
 rLine +="</p>";
-// console.log(rLine);
 
 }
 
 document.getElementById("upRight").innerHTML = rLine;
 }
 
+
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=pHeight;i > 0;i--){
-rLine +="<p>";
+var rLine  ="";
+       for (let j = 1; j <= i; j++) {
+mes  +="<p>";
     
 //Create each line on the Rhombus
-for(j=0;j<i;j++){
+       for (let j = 1; j <= i; j++) {
 
 //Is the position even or odd so we change the color
-if (j%2)
+       if (j % 2 == 0) {
 //even
 rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
+ } else {
 //odd
 rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
+}
 }
 rLine +="</p>";
+
 // console.log(rLine);
 
 }
