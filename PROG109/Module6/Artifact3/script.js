f312165c6f953dfc6a887889b9089dca8b9cf790
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-02-09 21:36:46 -0800

    Update script.js

--- PROG109/Module6/Artifact3/script.js
+++ PROG109/Module6/Artifact3/script.js
@@ -5,8 +5,8 @@
 
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
     upRight(pHeight, pColorEven, pColorOdd, pSymbol);
-    downRight(pHeight, pColorEven, pColorOdd, pSymbol);
     upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+    downRight(pHeight, pColorEven, pColorOdd, pSymbol);
     downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 }
 
@@ -34,6 +34,32 @@ rLine +="</p>";
     document.getElementById("upRight").innerHTML = rLine;
 }
 
+------
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+var rLine = "";
+for (i = 0; i < pHeight; i++) {
+rLine += "<p>";
+//Create each line on the Rhombus
+for (j = 0; j <= i; j++) {
+
+//Is the position even or odd so we change the color
+if (j % 2)
+//even
+rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+else
+//odd
+rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+}
+rLine += "</p>";
+// console.log(rLine);
+
+    }
+
+    document.getElementById("upLeft").innerHTML = rLine;
+}
+
+------
 
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol) {
 var rLine = "";
@@ -59,30 +85,7 @@ rLine += "</p>";
     document.getElementById("downRight").innerHTML = rLine;
 }
 
-
-function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
-var rLine = "";
-for (i = 0; i < pHeight; i++) {
-rLine += "<p>";
-//Create each line on the Rhombus
-for (j = 0; j <= i; j++) {
-
-//Is the position even or odd so we change the color
-if (j % 2)
-//even
-rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-else
-//odd
-rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-}
-rLine += "</p>";
-// console.log(rLine);
-
-    }
-
-    document.getElementById("upLeft").innerHTML = rLine;
-}
-
+------
 
 function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
 var rLine = "";
@@ -98,3 +101,12 @@ rLine += "<p>";
  else
  //odd
  rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+
+        }
+rLine += "</p>";        
+ // console.log(rLine);
+
+    }
+
+    document.getElementById("downLeft").innerHTML = rLine;
+}
