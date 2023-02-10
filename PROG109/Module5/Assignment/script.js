c2ed373908ee218bee57ec487db152b1b7b5c64d
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-02-09 23:02:15 -0800

    Update script.js

--- PROG109/Module5/Assignment/script.js
+++ PROG109/Module5/Assignment/script.js
@@ -1,14 +1,11 @@
-/*asks user for number, table unit*/
 let table;
 do { table = prompt ("Give me a number between 0 and 10?");
-} while (Number(table) === NaN || table < 0 || table > 10)
-  
-  /*initial value*/
-var i = 1;                
+var operator = 'multiplication';
+var i = 1;               
 var msg = "<h2>Multiplication Table</h2>";   
 
 if (operator === 'multiplication') {
-    while (i < 11) {
+    while (i < 10) {
         msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
         i++;
     }
