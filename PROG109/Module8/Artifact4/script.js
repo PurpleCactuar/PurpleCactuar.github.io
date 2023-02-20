d27c4217d4a1590aaaff6d4fc9d76b7ab5b8406e
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-02-19 21:11:56 -0800

    Update script.js

--- PROG109/Module8/Artifact4/script.js
+++ PROG109/Module8/Artifact4/script.js
@@ -1,3 +1,16 @@
+function validateForm(){
+//1) create a variable to control status of each field. Assume that they are not valid
+
+//2) create variables to read the values from html text inputs
+
+//3) do the validation
+
+//4) send error messages 
+
+//5) return the status of each field
+
+}
+
 var frmvalidator = new Validator("myform");
  frmvalidator.addValidation("FirstName","req","Please enter your First Name");
  frmvalidator.addValidation("FirstName","maxlen=20",
