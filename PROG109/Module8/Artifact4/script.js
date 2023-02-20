a43c388e1e58e2340d1f731dc0dc5b7589b0edb1
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-02-19 21:08:04 -0800

    Update script.js

--- PROG109/Module8/Artifact4/script.js
+++ PROG109/Module8/Artifact4/script.js
@@ -1,1 +1,32 @@
-sfgnsn
+var frmvalidator = new Validator("myform");
+ frmvalidator.addValidation("FirstName","req","Please enter your First Name");
+ frmvalidator.addValidation("FirstName","maxlen=20",
+		"Max length for FirstName is 20");
+
+ frmvalidator.addValidation("LastName","req");
+ frmvalidator.addValidation("LastName","maxlen=20");
+
+ frmvalidator.addValidation("Email","maxlen=50");
+ frmvalidator.addValidation("Email","req");
+ frmvalidator.addValidation("Email","email");
+
+ frmvalidator.addValidation("Phone","maxlen=50");
+ frmvalidator.addValidation("Phone","numeric");
+
+ frmvalidator.addValidation("Address","maxlen=50");
+ frmvalidator.addValidation("Country","dontselect=000");
+        
+  function DoCustomValidation()
+    {
+        var frm = document.forms["myform"];
+        if(frm.FirstName.value == 'No')
+        {
+            sfm_show_error_msg("No! Avada Kedavra!!");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
