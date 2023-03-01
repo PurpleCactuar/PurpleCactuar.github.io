a9083d29d98e3110906c63d9284d041390863ecc
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-02-28 17:44:17 -0800

    Update script.js

--- PROG109/Module9/Artifact5/script.js
+++ PROG109/Module9/Artifact5/script.js
@@ -1,11 +1,11 @@
-var myImages =["https://www.imms.org/images/dolphin_presentation/dolphin1.jpg", 
-               "http://cdn3.theeventchronicle.com/wp-content/uploads/2014/09/dolphin-intelligence.jpg",
-               "http://www.liveanimalslist.com/mammals/images/dolphin-in-water.jpg",
-               "http://i1151.photobucket.com/albums/o632/bowietvc15/Bowietvc15-2/DolphinsWallpaper2.jpg",
-               "http://i1151.photobucket.com/albums/o632/bowietvc15/funny%20gifs/animal-art-cute-dolphin-nature-Favimcom-311780.jpg"];
+var myImages =["Images/Xmas Fambam.jpeg", 
+               "Images/Binx Xmas.jpeg",
+               "Images/Kuma Car.jpeg",
+               "Images/Binx and Kuma.jpeg",
+               "Images/Izzy Toy Bin.jpeg"];
 
 
-var captionImages =["Laugh","Love","Lunch","Happiness","Family"];
+var captionImages =["Christmas 2020","Binx, the present destroyer","Kuma in her car","Favorite picture of them","Newest edition, Izzy"];
 
  var index=0; 
 
