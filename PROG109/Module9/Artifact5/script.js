472660105733df9105936c1e2d9d7c869b6ddb4b
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-02-28 21:22:47 -0800

    Update script.js

--- PROG109/Module9/Artifact5/script.js
+++ PROG109/Module9/Artifact5/script.js
@@ -1,31 +1,43 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var myImages =["https://PurpleCactuar.github.io/PROG109/Module9/Artifact5/Images/Binx Xmas1.jpeg",
+               "https://PurpleCactuar.github.io/PROG109/Module9/Artifact5/Images/Kuma Car1.jpeg",
+               "https://PurpleCactuar.github.io/PROG109/Module9/Artifact5/Images/Binx and Kuma.jpeg",
+               "https://PurpleCactuar.github.io/PROG109/Module9/Artifact5/Images/Izzy Toy Bin1.jpeg",
+               "https://PurpleCactuar.github.io/PROG109/Module9/Artifact5/Images/Izzy Tongue1.jpeg"];
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+var captionImages =["Binx, the destroyer.","Kuma, in her car.","Renaissance painting of Binx and Kuma","Newest edition, Izzy!","She's a weird sleeper."];
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+ var index=0; 
+
+ function updateImage(){
+ document.getElementById("slideshow").src = myImages[index];
+ document.getElementById("slideshow").alt= captionImages[index];
+ document.getElementById("caption").textContent = captionImages[index]; 
+} 
+
+function next(){
+ if (myImages.length == index+1)
+ index=0;
+ else
+ index++;
+ updateImage();
+} 
+ 
+
+function back(){
+ if (index===0)
+ index=myImages.length-1;
+ else
+ index--;
+ 
+ updateImage();
+} 
+
+var nextButton = document.getElementById("next"); 
+var previousButton = document.getElementById("previous"); 
+
+previousButton.addEventListener("click",back,false);
+nextButton.addEventListener("click",next,false); 
 
 function autoSlide(){
 if (document.getElementById("auto").checked)
