143fa9a49875c012217389f7fd4afbbb3b5db2a3
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-04-16 17:44:05 -0700

    Update script.js

--- PROG209/Week2/Assignment/script.js
+++ PROG209/Week2/Assignment/script.js
@@ -1,15 +1,11 @@
-/* the < fieldset > element provides a grouping for a part of an HTML form, 
-with a nested < legend > element providing a caption for the<fieldset>.
-It takes few attributes, the most notable of which are form, 
-which can contain the id of a < form > on the same page, allowing you to 
-make the < fieldset > part of that < form > even if it is not nested inside it, 
-and disabled, which allows you to disable the < fieldset > and all its contents in one go.
-https://developer.mozilla.org/en-us/docs/web/html/element/fieldset
-*/
+let orderNumber = 1000;
+
 document.addEventListener("DOMContentLoaded", function (event) {
-    // disable the lower part of the form until we have user input for all values
+        // disable the lower part of the form until we have user input for all values
     document.getElementById("experience").disabled = true;
+   
 })
+
 
 function formSubmitEvent() {
     let name = document.getElementById("custname").value;
@@ -21,9 +17,24 @@ function formSubmitEvent() {
     // we could do much better validation, but that's not the point of this example!
     if (name.length > 2 && address.length > 2 && city.length > 2 && state.length > 1 && zip.length == 5) {
         document.getElementById("experience").disabled = false;
-        document.getElementById("cust").value = name;
+        document.getElementById("receipt").value = ++orderNumber;
         document.getElementById("visitdate").value = new Date();
     }
     else {
         alert("please fill in all fields");
     }
+}
+
+function handleClick(RadioElement) {
+    // hanlde select value
+    var sel = document.getElementById('ordertype');
+
+    // handle radio button
+    var selectedValue = RadioElement.value;
+    if (selectedValue == "yes") {
+        alert("Thank you for your " + sel.value + " order.");
+    }
+    else {
+        alert("Please call customer service at 800-555-5555 and give them this order number: " + orderNumber);
+    }
+}
